c61d0bbcac8814d3dae467b0cab3ac2ef88fc02d
imkdw <imkdw@kakao.com>
2024-03-24 18:29:17 +0900

    feat: 에디터 비활성화

--- containers/article/new/contentEditor.tsx
+++ containers/article/new/contentEditor.tsx
@@ -1,5 +1,6 @@
-import ReactQuill from 'react-quill';
-import 'react-quill/dist/quill.snow.css';
+'use client';
+
+import { ChangeEvent } from 'react';
 
 interface Props {
   content: string;
@@ -7,47 +8,52 @@ interface Props {
 }
 
 export default function ArticleContentEditor({ content, changeHandler }: Props) {
-  const changeContentHandler = (value: string) => {
-    changeHandler(value);
+  const changeContentHandler = (event: ChangeEvent<HTMLTextAreaElement>) => {
+    changeHandler(event.target.value);
   };
 
-  const modules = {
-    toolbar: [
-      [{ header: [2, 3, 4, 5, false] }],
-      [{ color: [] }, { background: [] }],
-      ['bold', 'underline', 'strike', 'blockquote'],
-      [{ list: 'ordered' }, { list: 'bullet' }, { indent: '-1' }, { indent: '+1' }],
-      ['link', 'image'],
-      ['clean'],
-    ],
-  };
+  // const modules = {
+  //   toolbar: [
+  //     [{ header: [2, 3, 4, 5, false] }],
+  //     [{ color: [] }, { background: [] }],
+  //     ['bold', 'underline', 'strike', 'blockquote'],
+  //     [{ list: 'ordered' }, { list: 'bullet' }, { indent: '-1' }, { indent: '+1' }],
+  //     ['link', 'image'],
+  //     ['clean'],
+  //   ],
+  // };
 
-  const formats = [
-    'font',
-    'size',
-    'header',
-    'color',
-    'background',
-    'bold',
-    'italic',
-    'underline',
-    'strike',
-    'blockquote',
-    'list',
-    'bullet',
-    'indent',
-    'link',
-    'image',
-  ];
+  // const formats = [
+  //   'font',
+  //   'size',
+  //   'header',
+  //   'color',
+  //   'background',
+  //   'bold',
+  //   'italic',
+  //   'underline',
+  //   'strike',
+  //   'blockquote',
+  //   'list',
+  //   'bullet',
+  //   'indent',
+  //   'link',
+  //   'image',
+  // ];
 
   return (
-    <ReactQuill
-      theme="snow"
+    // <ReactQuill
+    //   theme="snow"
+    //   value={content}
+    //   onChange={changeContentHandler}
+    //   style={{ height: '600px' }}
+    //   modules={modules}
+    //   formats={formats}
+    // />
+    <textarea
       value={content}
       onChange={changeContentHandler}
-      style={{ height: '600px' }}
-      modules={modules}
-      formats={formats}
+      className="h-[500px] border border-gray-300 p-2 outline-none"
     />
   );
 }
