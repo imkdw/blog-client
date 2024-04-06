579e0cd6690279fd7cc09ed3551883c6e549b217
imkdw <imkdw@kakao.com>
2024-04-06 17:36:39 +0900

    feat: 게시글 조회수 추가 및 중복조회 방지로직 추가

--- app/articles/[slug]/page.tsx
+++ app/articles/[slug]/page.tsx
@@ -31,13 +31,11 @@ interface Props {
 }
 
 export default async function ArticleDetailPage({ params: { slug } }: Props) {
-  const articleId = slug;
-
   const cookieStore = cookies();
   const accessToken = cookieStore.get('accessToken')?.value || '';
 
-  const articleDetail = await getArticleDetail(articleId, accessToken);
-  const articleTags = await getArticleTags(articleId);
+  const articleDetail = await getArticleDetail(slug, accessToken);
+  const articleTags = await getArticleTags(slug);
 
   return (
     <div className="flex h-auto w-full flex-col items-center gap-10">
@@ -48,20 +46,20 @@ export default async function ArticleDetailPage({ params: { slug } }: Props) {
             title={articleDetail.title}
             summary={articleDetail.summary}
             content={articleDetail.content}
-            articleId={articleId}
+            articleId={slug}
           />
           <ArticleTags createAt={articleDetail.createdAt} tags={articleTags.tags} />
           <ArticleUtilButtons
             commentCount={articleDetail.commentCount}
             _likeCount={articleDetail.like.likeCount}
             _isLike={articleDetail.like.isLiked}
-            articleId={articleId}
+            articleId={slug}
             summary={articleDetail.summary}
             thumbnail={articleDetail.thumbnail}
             title={articleDetail.title}
           />
-          <Comments articleId={articleId} />
-          <CommentWriteForm articleId={articleId} />
+          <Comments articleId={slug} />
+          <CommentWriteForm articleId={slug} />
         </>
       )}
     </div>
