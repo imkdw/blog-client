324946e1d8c383b0f5d131835db2af0efa2514c0
imkdw <imkdw@kakao.com>
2024-04-08 22:54:07 +0900

    feat: 모바일뷰 사이드메뉴 로그아웃버튼 추가

--- components/sidemenu/profile.tsx
+++ components/sidemenu/profile.tsx
@@ -1,11 +1,20 @@
 'use client';
 
 import { AccountCircle } from '@mui/icons-material';
+import Link from 'next/link';
+import { MouseEvent } from 'react';
+
 import useUser from '../../store/use-user';
+import useSidemenu from '../../store/use-sidemenu';
 
 export default function Profile() {
-  const { isLoggedIn } = useUser((state) => state);
-  const { loggedInUser } = useUser((state) => state);
+  const { isLoggedIn, loggedInUser } = useUser((state) => state);
+  const { setIsOpen } = useSidemenu((state) => state);
+
+  const clickHandler = (event: MouseEvent<HTMLAnchorElement>) => {
+    event.stopPropagation();
+    setIsOpen(false);
+  };
 
   return (
     <div className="flex h-[150px] w-full flex-row items-end justify-center border-b border-r-gray-300 pb-5">
@@ -17,7 +26,9 @@ export default function Profile() {
             <p className="text-left text-gray-400">{loggedInUser.email}</p>
           </div>
         ) : (
-          <p className="text-gray-400">로그인이 필요한 서비스입니다</p>
+          <Link className="text-gray-400" href="/auth/sign-in" onClick={clickHandler}>
+            로그인이 필요한 서비스입니다
+          </Link>
         )}
       </div>
     </div>
--- components/sidemenu/sidemenu.tsx
+++ components/sidemenu/sidemenu.tsx
@@ -1,19 +1,38 @@
 'use client';
 
 import { MouseEvent } from 'react';
-import { Close } from '@mui/icons-material';
+import { Close, Logout } from '@mui/icons-material';
+import { useRouter } from 'next/navigation';
 
 import Profile from './profile';
 import MenuSearch from './search';
 import SideMenuLinks from './links';
 import useSidemenu from '../../store/use-sidemenu';
+import useUser from '../../store/use-user';
+import { UserRoles } from '../../types/enum/user';
 
 export default function SideMenu() {
   const { setIsOpen } = useSidemenu((state) => state);
+  const { setIsLoggedIn, setLoggedInUser, isLoggedIn } = useUser((state) => state);
+  const router = useRouter();
 
   const closeHandler = (event: MouseEvent) => {
     event.stopPropagation();
     setIsOpen(false);
+  };
+
+  const logoutHandler = async (event: MouseEvent<HTMLButtonElement>) => {
+    event.stopPropagation();
+    setIsLoggedIn(false);
+    setLoggedInUser({
+      email: '',
+      nickname: '',
+      profile: '',
+      role: UserRoles.NORMAL,
+    });
+    useUser.persist.clearStorage();
+    setIsOpen(false);
+    router.push('/');
   };
 
   return (
@@ -22,6 +41,16 @@ export default function SideMenu() {
       <Profile />
       <MenuSearch />
       <SideMenuLinks />
+      {isLoggedIn && (
+        <button
+          type="button"
+          className="absolute bottom-5 right-5 flex items-center gap-2 text-xl text-gray-500"
+          onClick={logoutHandler}
+        >
+          <Logout fontSize="medium" className="text-gray-400" />
+          <p>로그아웃</p>
+        </button>
+      )}
     </div>
   );
 }
